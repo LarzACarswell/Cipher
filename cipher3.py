3773aab7e02478e69c8066d4dfc65a794f799a34
Larz Carswell <LarzACarswell@users.noreply.github.com>
2017-02-23 01:18:29 -0500

    Add files via upload

--- cipher3.py
+++ cipher3.py
@@ -2,14 +2,7 @@ import string
 import math
 #alp = string.ascii_letters[0:26]+string.digits+string.whitespace+"'.;:!@#$%^&*()_-+="
 alp=string.printable
-"""mesg = Complete and submit the following lab assignment for grading by your instructor.
-Make sure that you complete and answer all questions.
-Record your answers on this sheet and hand in this completed exercise sheet with screen shots of each step.
-Use MS Word only to record your answers, and screen shots.
-Save the document with the filename Lab1WiresharkXXX.
-Substitute your first, middle, and last name initials in place of XXX.  """
-#mesg1="hello"
-#key1=10
+
 def encrypt(str1, key):
     offset = key
     tempstring = ""
@@ -46,6 +39,4 @@ def decrypt(str2, key):
                 offset+=1
                 tempstring2+=alp[(j-int(math.floor(15*math.sin(offset))))%len(alp)]
     return tempstring2
-"""enc=encrypt(mesg,key1)
-dec=decrypt(enc,key1)
-print enc+"\n-------------------------\n"+dec"""
+
